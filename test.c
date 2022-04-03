75512450261a8504b7eb6500383ae3d1d877ab44
Corey Green <king.dg28@gmail.com>
2022-04-03 16:23:57 -0500

    progress

--- test.c
+++ test.c
@@ -2,7 +2,10 @@
 //Corey Green.
 //3-23-22
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <unistd.h>  
+#include <semaphore.h>
+#include <pthread.h>
 /*
 INPUTS:
 
@@ -14,19 +17,30 @@ INPUTS:
 # patient checkup time
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>  
-#include <pthread.h>
 
-void *patientThread();
+void *patientThreadFunc();
+void *staffThreadFunc();
+void enterWaitingRoom();
+void sitOnSofa();
+void getMedicalCheckup();
+void makePayment();
+void leaveClinic();
+void waitForPatients();
+void performMedicalCheckup();
+void acceptPayment();
 
+int successfulCheckups;
+int avgStaffWaitTime;
+int patientsLeft;
+int avgPatientsWaitTime;
+sem_t mutex;
+int totalRoomCapacity;
+int totalSofaCapacity;
 
 // allow command line args
 int main(int argc, char *argv[])
 {
-    pthread_t thread1;
-
+    //assigns the arguments to ints
     int medicalStaff, totalPatients, roomCapacity, sofaSpace, maxTimeInterval, checkupTime;
 
     	medicalStaff = atoi(argv[1]);
@@ -36,21 +50,102 @@ int main(int argc, char *argv[])
 	    maxTimeInterval = atoi(argv[5]);
 	    checkupTime = atoi(argv[6]);
 
-        pthread_create(&thread1, NULL, &patientThread, NULL);
-        pthread_join(thread1, NULL);
+        totalRoomCapacity = roomCapacity;
+        totalSofaCapacity = sofaSpace;
+
+        //initializes threads for staff & patients
+        pthread_t patientThread[totalPatients];
+        pthread_t staffThread[medicalStaff];
+
+        //for loops for the creation and joining of patient and staff threads
+        //TODO: put these into functions with int args.
+        for (int j = 0; j < medicalStaff; j++)
+        {
+        pthread_create(&staffThread[j], NULL, &staffThreadFunc, NULL);
+        printf("Medical Professional %d (Thread ID: %ld): Waiting for patient \n", j, (long)staffThread[j]);
+        }
+        for (int i = 0; i < totalPatients; i++)
+        {
+        pthread_create(&patientThread[i], NULL, &patientThreadFunc, NULL);
+        printf("Patient %d (Thread ID: %ld Arrived to clinic\n", i, (long)patientThread[i]);
+        }
+        for (int j = 0; j < medicalStaff; j++)
+        {
+        pthread_join(staffThread[j], NULL);
+        }
+        for (int i = 0; i < totalPatients; i++)
+        {
+        pthread_join(patientThread[i], NULL);
+        }
+    
 
 //printf("medicalstaff = %d, totalPatients = %d, roomCapacity = %d, sofaSpace = %d, maxTimeInterval = %d, checkupTime = %d", medicalStaff, totalPatients, roomCapacity, sofaSpace, maxTimeInterval, checkupTime);
  
     return 0;
 }
 
-void *patientThread(void *vargp)
+void *staffThreadFunc(void *vargp)
 {
-    printf("hola");
-    //TODO: enterWaitingRoom, 
-    //TODO: sitOnSofa, 
-    //TODO: getMedicalCheckup, 
-    //TODO: makePayment, 
-    //TODO: leaveClinic 
+    waitForPatients();
+    performMedicalCheckup();
+    acceptPayment();
         return NULL;
+}
+void *patientThreadFunc(void *vargp)
+{
+    enterWaitingRoom();
+    sitOnSofa();
+    getMedicalCheckup();
+    makePayment();
+    leaveClinic();
+        return NULL;
+}
+
+//MARK: funcs used by used by patients
+void enterWaitingRoom()
+{
+    sem_wait(&mutex);
+
+    if (totalRoomCapacity > 0)
+    {
+        totalRoomCapacity--;
+        sem_post(&mutex);
+    }
+    else
+    {
+        sem_post(&mutex);
+        leaveClinic();
+    }
+    //print()
+}
+void sitOnSofa()
+{
+    
+    //TODO: setup
+}
+void getMedicalCheckup()
+{
+    //TODO: setup
+}
+void makePayment()
+{
+    //TODO: setup
+}
+void leaveClinic()
+{
+    //TODO: setup
+}
+
+//MARK: funcs used by used by staff
+void waitForPatients()
+{
+    //TODO: setup
+}
+void performMedicalCheckup()
+{
+    //TODO: setup
+}
+void acceptPayment()
+{
+    //TODO: setup
 }
